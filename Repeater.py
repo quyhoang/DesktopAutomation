54f2ce5a6b7588e7b830c93c83e8a139cc01363d
quyhoang <dear.quyhoang@gmail.com>
2023-08-17 07:52:50 +0900

    Allow temporary suspension

--- Repeater.py
+++ Repeater.py
@@ -1,18 +1,43 @@
+from operator import truediv
 import keyboard
 import threading
 import time
 import pygetwindow as gw
 from plyer import notification
 from prettytable import PrettyTable
+import tkinter as tk
+from tkinter import messagebox
+
 
 run_flag = False
 interval_seconds = 0
 start_time = None
 max_iterations = 10
 initial_active_window  = None
+suspend = False
 
 current_thread = None
 terminate_thread = threading.Event() # Event to signal thread termination
+
+def suspendProgram():
+    global suspend
+    if suspend == True:
+        suspend = False
+        print("You can now use section repeater.")
+        # msgbox("Suspended", "Section repeater is suspended.")
+        return
+    if suspend == False:
+        suspend = True
+        print("Section repeater is suspended.")
+        # msgbox("Unsuspended", "You can now use section repeater.")
+        return
+
+def msgbox(title, message):
+    root = tk.Tk()
+    root.withdraw() # Hide the main window
+    messagebox.showinfo(title, message)
+    root.update() # Update the main window
+    root.destroy() # Close the main window
 
 def repeat10more():
     global run_flag
@@ -68,6 +93,7 @@ def startSingleThreadLoop():
 
 
 def loopVideo():
+    global suspend
     global initial_active_window  #only send input in video windows
 
     global run_flag 
@@ -75,13 +101,13 @@ def loopVideo():
 
     global start_time
 
-
-    print(f'Looping {interval_seconds:.2f} seconds')
+    if not suspend:
+        print(f'Looping {interval_seconds:.2f} seconds')
 
     global max_iterations
     iterations = 0
 
-    while run_flag and not terminate_thread.is_set(): # Check for termination signal
+    while run_flag and not terminate_thread.is_set() and not suspend: # Check for termination signal
         current_active_window = gw.getActiveWindow()
         if current_active_window != initial_active_window:
             initial_active_window.activate()
@@ -114,6 +140,7 @@ def main():
     # )
 
     # print('\nビデオを通じて学ぶためのセクションリピーターのユーティリティへようこそ!\n')
+    msgbox("Welcome", "Section repeater has started")
     print("\n--------------------------------------")
     print(' Welcome to Section Repeater Utility!')
     print("--------------------------------------\n")
@@ -122,9 +149,9 @@ def main():
     
 
     table = PrettyTable()
-    table.add_column("Shortcut", ["W", "E", "R", "T","Delete","]"]) 
-    table.add_column("Repeater Function", ["Mark beginning", "Mark ending/ start looping", "Repeat 10 more times", "Exit loop", "Exit program", ""])
-    table.add_column("Video Speed Controller Function", ["Set marker", "", "", "", "", "Jump to marker"])
+    table.add_column("Shortcut", ["W", "E", "R", "T", "Ctrl + F3","Delete","]"]) 
+    table.add_column("Repeater Function", ["Mark beginning", "Mark ending/Start looping", "Repeat 10 more times", "Exit loop", "Suspend/Unsuspend", "Exit program", ""])
+    table.add_column("Video Speed Controller Function", ["Set marker", "", "", "", "", "", "Jump to marker"])
 
     print(table)
 
@@ -132,15 +159,18 @@ def main():
     keyboard.add_hotkey('t', terminate)
     keyboard.add_hotkey('e', lambda: threading.Thread(target=startSingleThreadLoop).start())
     keyboard.add_hotkey('w', setStartPoint)
+    keyboard.add_hotkey('Ctrl + F3', suspendProgram)
     keyboard.wait('Delete')
+  
     
+
+
+
+if __name__ == '__main__':
+    main()
     # notification.notify(
     # title='またね！',
     # message='The program will exit',
     # app_name='Section repeater',
     # timeout=3  # The notification will be visible for 3 seconds
     # )
-
-
-if __name__ == '__main__':
-    main()
